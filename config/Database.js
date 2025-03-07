107b5bb91cfcb7a9ba5d028b53dba63687e3eee7
Dipesh <dipesh.stha70@gmail.com>
2025-03-07 12:57:18 +0545

    fixed the mongodb connection

--- config/Database.js
+++ config/Database.js
@@ -3,8 +3,8 @@ const { default: mongoose } = require('mongoose');
 let dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/auth_project';
 
 exports.dbConnect = async () => {
-  mongoose
-    .connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+  return await mongoose
+    .connect(dbUrl)
     .then(() => console.log('Connected to MongoDB'))
     .catch((error) => console.log('MongoDB Connection Error:', error));
 };
